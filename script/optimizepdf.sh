5fb65e46736511188ea3bfe225a8f4554948dd11
Carl Janzen <carl.janzen@gmail.com>
2017-07-03 09:04:45 -0700

    cleanup

--- script/optimizepdf.sh
+++ script/optimizepdf.sh
@@ -5,9 +5,6 @@ dn=$(dirname "${1}")
 tmpname="${dn}/tmp.${bn}"
 # from: http://stackoverflow.com/questions/10450120/optimize-pdf-files-with-ghostscript-or-other
 mv "${1}" "${tmpname}"
-echo in: ${1}
-echo tmp: ${tmpname}
-ls build
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/data/${1} /data/${tmpname}
 rm -f "${tmpname}"
 [ $# -gt 1 ] && { shift ; ${0} $* ; }
